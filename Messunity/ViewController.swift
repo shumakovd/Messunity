6591e3b9706cbca94c1e47ca4554f5b35a9ef8e3
shumakovd <shumakov_d@hotmail.com>
2022-11-13 16:05:15 +0200

    Initial Commit

---
+++ Messunity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messunity
+//
+//  Created by Shumakov Dmytro on 13.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
